679a431d1bb8754d64a373e9702b4bd3fe108a02
Genskoria <Genskoria@Genskoria-PC>
2016-10-20 21:47:53 -0400

    .

---
+++ src/johnbot/JohnBot.java
@@ -0,0 +1,14 @@
+package johnbot;
+
+import javax.swing.JOptionPane;
+
+public class JohnBot {
+
+    public static void main(String[] args) {
+
+         JOptionPane.showInputDialog("Cual es su pregunta?");
+         JOptionPane.showMessageDialog(null, "No se.");
+        
+    }
+    
+}
